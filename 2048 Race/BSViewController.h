f37435a8844d79b794d86c658004b021a6cfe0b0
Nathan Pabrai <nathanpabrai@gmail.com>
2014-03-21 01:57:37 -0400

    First Commit
    Move to left, only movement works

--- 2048 Race/BSViewController.h
+++ 2048 Race/BSViewController.h
@@ -9,5 +9,22 @@
 #import <UIKit/UIKit.h>
 
 @interface BSViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *tile1;
+@property (weak, nonatomic) IBOutlet UILabel *tile2;
+@property (weak, nonatomic) IBOutlet UILabel *tile3;
+@property (weak, nonatomic) IBOutlet UILabel *tile4;
+@property (weak, nonatomic) IBOutlet UILabel *tile5;
+@property (weak, nonatomic) IBOutlet UILabel *tile6;
+@property (weak, nonatomic) IBOutlet UILabel *tile7;
+@property (weak, nonatomic) IBOutlet UILabel *tile8;
+@property (weak, nonatomic) IBOutlet UILabel *tile9;
+@property (weak, nonatomic) IBOutlet UILabel *tile10;
+@property (weak, nonatomic) IBOutlet UILabel *tile11;
+@property (weak, nonatomic) IBOutlet UILabel *tile12;
+@property (weak, nonatomic) IBOutlet UILabel *tile13;
+@property (weak, nonatomic) IBOutlet UILabel *tile14;
+@property (weak, nonatomic) IBOutlet UILabel *tile15;
+@property (weak, nonatomic) IBOutlet UILabel *tile16;
+@property (strong,nonatomic) NSMutableArray *tiles;
 
 @end
--- 2048 Race/BSViewController.m
+++ 2048 Race/BSViewController.m
@@ -18,8 +18,125 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  [self setUpGestureRecognizers];
+  [self setUpTiles];
+  
+  UIAlertView *alert = [[UIAlertView alloc]
+                        
+                        initWithTitle:@"How To Play"
+                        message:@"Insert\n\n\n\nGameplay Instructions"//Need to finalize gameplay instructions here!
+                        delegate:nil
+                        cancelButtonTitle:@"Start!"
+                        otherButtonTitles:nil];
+  [alert show];
+  
 }
 
+-(void)setUpGestureRecognizers{
+  UISwipeGestureRecognizer* swipeUpGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeUpFrom:)];
+  swipeUpGestureRecognizer.direction = UISwipeGestureRecognizerDirectionUp;
+  [self.view addGestureRecognizer:swipeUpGestureRecognizer];
+  UISwipeGestureRecognizer* swipeDownGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeDownFrom:)];
+  swipeDownGestureRecognizer.direction = UISwipeGestureRecognizerDirectionDown;
+  [self.view addGestureRecognizer:swipeDownGestureRecognizer];
+  UISwipeGestureRecognizer* swipeLeftGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeLeftFrom:)];
+  swipeLeftGestureRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
+  [self.view addGestureRecognizer:swipeLeftGestureRecognizer];
+  UISwipeGestureRecognizer* swipeRightGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeRightFrom:)];
+  swipeRightGestureRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
+  [self.view addGestureRecognizer:swipeRightGestureRecognizer];
+}
+-(void)setUpTiles{
+  self.tiles=[[NSMutableArray alloc] init];
+  //add the tiles to the tiles array
+  [self.tiles addObject:self.tile1];
+  [self.tiles addObject:self.tile2];
+  [self.tiles addObject:self.tile3];
+  [self.tiles addObject:self.tile4];
+  [self.tiles addObject:self.tile5];
+  [self.tiles addObject:self.tile6];
+  [self.tiles addObject:self.tile7];
+  [self.tiles addObject:self.tile8];
+  [self.tiles addObject:self.tile9];
+  [self.tiles addObject:self.tile10];
+  [self.tiles addObject:self.tile11];
+  [self.tiles addObject:self.tile12];
+  [self.tiles addObject:self.tile13];
+  [self.tiles addObject:self.tile14];
+  [self.tiles addObject:self.tile15];
+  [self.tiles addObject:self.tile16];
+  //initialize tile values and pick 2 random starting tiles
+  for (UILabel* tile in self.tiles){
+    [tile setText:@""];
+  }
+  [(id)self.tiles[arc4random()%16] setText:@"2"] ;
+  
+  [self addRandomTile];
+}
+
+-(void)addRandomTile{
+  int tmp=arc4random()%16;
+  
+  while ([[(id)self.tiles[tmp] text]intValue])
+  {
+    tmp=arc4random()%16;
+  }
+  
+  [(id)self.tiles[tmp] setText:@"2"];
+}
+
+- (void)handleSwipeUpFrom:(UIGestureRecognizer*)recognizer {
+  NSLog(@"Swiped Up :)");
+  
+  
+}
+- (void)handleSwipeDownFrom:(UIGestureRecognizer*)recognizer {
+  NSLog(@"Swiped Down :)");
+  
+}
+- (void)handleSwipeLeftFrom:(UIGestureRecognizer*)recognizer {
+  NSLog(@"Swiped Left :)");
+  for (int m=0;m<4;m++){
+  for (int i=1+4*m;i<4+4*m;i++){
+    if ([[self.tiles[i] text]intValue]) {
+      //The tile has a value
+        //Tile not at boundry
+        if ([[self.tiles[i-1] text]intValue]) {
+          //If the tile to left has a value
+          //...
+          if ([[self.tiles[i-1] text]intValue]==[[self.tiles[i] text]intValue] ) {
+            //if the one on the left matches
+          }
+        }
+        else{
+          //move the tile to the left
+          [self moveLeftfrom:i withEdge:4*m];
+          
+        }//ends else
+      
+      
+    }//else the tile is empty
+    
+  }
+  }
+  
+}
+
+-(void)moveLeftfrom: (int) position withEdge: (int) edge{
+  //this function moves the tile to the leftmost position through free space
+  int j=0;
+  int i=position;
+  while((i-j-1>edge-1)&&([[self.tiles[i-j-1] text] intValue ]==0)){
+    [self.tiles[i-j-1] setText:[self.tiles[i-j] text]];
+    [self.tiles[i-j] setText:@""];
+    j++;
+  }//ends while
+}
+
+- (void)handleSwipeRightFrom:(UIGestureRecognizer*)recognizer {
+  NSLog(@"Swiped Right :)");
+  
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
